d482453688d34b5b3360a6743b5ca5348e9324cb
chaiwat <chaiwat.chaobaankoh@accenture.com>
2021-12-15 15:40:56 +0700

    update: get MIN_VAlUE each of file type

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -5,7 +5,7 @@ public class Utilities {
     // float, long, int
 
     public static float getFloatValue(String in) {
-        float out = 0.0f;
+        float out = Float.MIN_VALUE;
         try {
             out = Float.parseFloat(in);
         } catch (NumberFormatException e) {
@@ -15,18 +15,17 @@ public class Utilities {
     }
 
     public static long getLongValue(String in) {
-        long out = 0;
+        long out = Long.MIN_VALUE;
         try {
             out = Long.parseLong(in);
         } catch (NumberFormatException e) {
             System.out.println(in + " cannot be converted into a 'long' value. Exiting program.");
         }
         return out;
-
     }
 
     public static int getIntValue(String in) {
-        int out = 0;
+        int out = Integer.MIN_VALUE;
         try {
             out = Integer.parseInt(in);
         } catch (NumberFormatException e) {
