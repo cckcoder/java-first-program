89cea671e3be27d959a32428ae6504de64d1a71b
chaiwat <chaiwat.chaobaankoh@accenture.com>
2021-12-15 12:34:42 +0700

    fix: module 3

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -31,7 +31,7 @@ public class SavingsCalculator {
     }
 
     public float calculate() {
-        return sumOfDebits() - sumOfCredits();
+        return sumOfCredits() - sumOfDebits();
     }
 
     public static void main(String[] args) {
